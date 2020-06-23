6d536cb5d169f64edae3d1d7a2f9e65dd0ef11d6
Vasiliy Pupkin <vpupkin@company.com>
2020-06-23 19:37:09 +0300

    верстка

--- js/main.js
+++ js/main.js
@@ -22,9 +22,11 @@ $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
 
   if (wScroll > 20) {
-    $('.top-menu').addClass('active');
+    $('.top-header').addClass('active');
+    $('.social').addClass('active');
   } else {
-    $('.top-menu').removeClass('active');
+    $('.top-header').removeClass('active');
+    $('.social').removeClass('active');
   };
 });
 
@@ -48,20 +50,52 @@ var typed2 = new Typed('.typewrite', {
 let user_icon = document.querySelector('.burger');
 user_icon.addEventListener("click", function (e) {
   let user_menu = document.querySelector('.main-menu');
+  let user_social = document.querySelector('.social');
   user_menu.classList.toggle('show');
   user_icon.classList.toggle('active');
+  user_social.classList.toggle('show');
 });
 
 document.documentElement.addEventListener("click", function (e) {
   if (!e.target.closest('.burger')) {
     let user_menu = document.querySelector('.main-menu');
     let user_icon = document.querySelector('.burger');
+    let user_social = document.querySelector('.social');
     user_menu.classList.remove('show');
     user_icon.classList.remove('active');
+    user_social.classList.remove('show');
   }
 });
 
 $('.main-menu a').click(function () {
   $('.main-menu').removeClass('show');
   $('.burger').removeClass('active');
+  $('.social').removeClass('show');
 });
+
+/*************     изменение HTML      ******************/
+$(window).resize(function (event) {
+  adaptive_function();
+});
+
+function adaptive_header(w, h) {
+  var headerMenu = $('.main-menu'); /* куда закинуть блок */
+  var headerSocial = $('.social'); /* блок, который закинуть */
+  if (w < 800) {
+    /* ширина */
+    if (!headerSocial.hasClass('done')) {
+      headerSocial.addClass('done').appendTo(headerMenu);
+    }
+  } else {
+    if (headerSocial.hasClass('done')) {
+      headerSocial.removeClass('done').prependTo($('.nav')); /* вернуть блок на прежнее место */
+    }
+  }
+}
+
+function adaptive_function() {
+  var w = $(window).outerWidth();
+  var h = $(window).outerHeight();
+  adaptive_header(w, h);
+}
+adaptive_function();
